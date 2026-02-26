d52624d43c336a40192ddf32fb192bcf271544f6
yash kaushik <yashkaushikmbd7@gmail.com>
2026-02-26 15:58:58 +0530

    Fix vite config for Solana build

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
+import { Buffer } from 'buffer'
+window.Buffer = Buffer
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from "vite-plugin-node-polyfills";
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),
-    nodePolyfills(),
-  ],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+    },
+  },
+  optimizeDeps: {
+    include: ['buffer'],
+  },
+  define: {
+    global: 'globalThis',
+  },
 })
